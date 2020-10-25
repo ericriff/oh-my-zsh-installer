c26b9f7f690fff306aae777f53268c1d06521007
Riff, Eric <eriff@irobot.com>
2020-10-25 19:01:10 -0300

    Add debug messages

--- oh-my-zsh-installer.sh
+++ oh-my-zsh-installer.sh
@@ -1,17 +1,21 @@
 #! /usr/bin/env bash
 
 # Install zsh
+echo "+ Install ZSH"
 sudo apt-get update
 sudo apt-get install -y zsh
 
 # Install oh-my-zsh
+echo "+ Install oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install autosuggestions plugin
+echo "+ Install sutossugestions plug-in"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sed -i 's~plugins=(~plugins=(zsh-autosuggestions ~1' ~/.zshrc
 
 # Set agnoster theme
+echo "+ Set agnoster theme [and install powerline fonts]"
 sudo apt-get install fonts-powerline 
 sed -i 's~ZSH_THEME="robbyrussell"~ZSH_THEME="agnoster"~1' ~/.zshrc
 
